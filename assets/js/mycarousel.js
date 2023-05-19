00407a142e2265fd0737ce02305349af17f4dcc6
Laureano <ramonero27@gmail.com>
2023-05-19 18:28:19 -0300

    add new changes

--- assets/js/mycarousel.js
+++ assets/js/mycarousel.js
@@ -1,14 +1,20 @@
 const myDesktopImages=document.querySelectorAll('.sneaker-img-desktop');
+const myThumbs=document.querySelectorAll('.mythumb');
+const myThumbnailImages=document.querySelectorAll('.thumbnail-img-desktop');
 
 let indice=0;
 
 
 function mountSneakers(){
     myDesktopImages[indice].classList.add('mostrar-imagenes');
+    myThumbs[indice].style.border="2px solid var(--orange)";
+    myThumbnailImages[indice].style.filter="opacity(.4)";
 }
 
 const dismountSneakers=()=>{
     myDesktopImages[indice].classList.remove('mostrar-imagenes');
+    myThumbs[indice].style.border="none";
+    myThumbnailImages[indice].style.filter="opacity(1)";
 }
 
 document.getElementById('btn-next-desktop').addEventListener('click',()=>{
@@ -39,6 +45,32 @@ document.getElementById('btn-modal').addEventListener('click',()=>{
 
 document.querySelector('.main-img').addEventListener('click',()=>{
     document.querySelector('.dark-desktop').style.display="flex";
+});
+
+
+myThumbnailImages[0].addEventListener('click',()=>{
+    dismountSneakers();
+    indice=0;
+    mountSneakers();
+});
+
+myThumbnailImages[1].addEventListener('click',()=>{
+    dismountSneakers();
+    indice=1;
+    mountSneakers();
+});
+
+myThumbnailImages[2].addEventListener('click',()=>{
+    dismountSneakers();
+    indice=2;
+    mountSneakers();
+});
+
+
+myThumbnailImages[3].addEventListener('click',()=>{
+    dismountSneakers();
+    indice=3;
+    mountSneakers();
 });
 
 mountSneakers();
